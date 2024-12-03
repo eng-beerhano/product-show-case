85b0fe9468eae3adb3e684e0f245cefaef406bdf
hassan <hassan@gmail.com>
2024-12-03 22:05:56 +0300

    first commit

---
+++ HTML/js.js
@@ -0,0 +1,6 @@
+let myName= "Hassan";
+console.log("magacaygu waa +"myName"")
+let Age = 12;
+console.log("da'deydu waa" + Age)
+let isStudent = true;
+console.log(isStudent);
---
+++ src/java.js
@@ -0,0 +1,27 @@
+// Events
+
+// 1.Mouse events
+// a.     b.     c.
+// 2.keyboard events
+// a.     b.    c. 
+// 3.form events
+// let h1 = document.querySelector('h1')
+//  let body = document.querySelector('body')
+//  let prg = document.querySelector('p')
+// h1.addEventListener("mouseover", function(){
+//     h1.style.color = 'white'
+// })
+
+// body.style.backgroundColor = 'black'
+
+// h1.addEventListener('mouseleave',function(){
+//     h1.style.color= 'yellow'
+// })
+
+// prg.addEventListener('click', function(){
+//     prg.style.color = 'green'
+// })
+
+
+
+
